1158f0a5f8a7a0e31a5915e5e3361fac31dbd581
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-02-04 10:16:38 -0600

    Trying to add through directr command

--- src/sos_xeus_cling/kernel.py
+++ src/sos_xeus_cling/kernel.py
@@ -12,16 +12,30 @@ from collections import Sequence
 from IPython.core.error import UsageError
 import re
 
+cpp_init_statements = r'''
+#define typename(x) _Generic((x),                                                 \
+         bool: "_Bool",                  unsigned char: "unsigned char",          \
+         char: "char",                     signed char: "signed char",            \
+    short int: "short int",         unsigned short int: "unsigned short int",     \
+          int: "int",                     unsigned int: "unsigned int",           \
+     long int: "long int",           unsigned long int: "unsigned long int",      \
+long long int: "long long int", unsigned long long int: "unsigned long long int", \
+        float: "float",                         double: "double",                 \
+  long double: "long double",                   char *: "pointer to char",        \
+       void *: "pointer to void",                int *: "pointer to int",         \
+      default: "other")
+'''
+
 class sos_xeus_cling:
     supported_kernels = {'C++11': ['xeus-cling-cpp11'], 'C++14' : ['xeus-cling-cpp14'], 'C++17' : ['xeus-cling-cpp17']}
     background_color = {'C++11': '#DDA0DD', 'C++14': '#D8BFD8', 'C++17': '#E6E6FA'}
     options = {}
     cd_command = '#include <unistd.h>\nchdir("{dir}");'
 
-    def __init__(self, sos_kernel, kernel_name='scala'):
+    def __init__(self, sos_kernel, kernel_name='C++11'):
         self.sos_kernel = sos_kernel
         self.kernel_name = kernel_name
-        self.init_statements = '#import "utils.hpp"'
+        self.init_statements = cpp_init_statements
 
     def get_vars(self, names):
         for name in names:
