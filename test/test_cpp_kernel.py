26cf305c53c84434d9f3c06dd27812ca82a7db3d
Konstantin Taletskiy <konstantin@taletskiy.com>
2019-02-14 10:55:45 -0500

    Test for Dataframe transfer

--- test/test_cpp_kernel.py
+++ test/test_cpp_kernel.py
@@ -7,6 +7,7 @@ import os
 import unittest
 from ipykernel.tests.utils import assemble_output, execute, wait_for_idle
 from sos_notebook.test_utils import sos_kernel, get_result, get_display_data, clear_channels
+from time import sleep
 
 class TestCppKernel(unittest.TestCase):
 
@@ -32,9 +33,11 @@ class TestCppKernel(unittest.TestCase):
                 string1 = 'abc'
                 bool1 = True
                 ''')
-            clear_channels(iopub)
+            wait_for_idle(kc)
+
             execute(kc=kc, code='%use C++14')
             wait_for_idle(kc)
+
             execute(kc=kc, code='%get int1 int2 int4 float1 float2 float3 string1 bool1')
             wait_for_idle(kc)
 
@@ -47,11 +50,6 @@ class TestCppKernel(unittest.TestCase):
             execute(kc=kc, code='std::cout << int2;')
             stdout, _ = assemble_output(iopub)
             self.assertEqual(stdout.strip(),'1000000000000000000')
-
-            # #Test int2
-            # execute(kc=kc, code='std::cout << int2;')
-            # stdout, _ = assemble_output(iopub)
-            # self.assertEqual(stdout.strip(),'1000000000000000000')
 
             #Test int4
             execute(kc=kc, code='std::cout << int4;')
@@ -85,6 +83,29 @@ class TestCppKernel(unittest.TestCase):
 
             execute(kc=kc, code="%use sos")
             wait_for_idle(kc)
+
+    def testPythonToCppDataframe(self):
+        with sos_kernel() as kc:
+            iopub = kc.iopub_channel
+            execute(kc=kc, code = '''
+                import numpy as np
+                import pandas as pd
+                dataframe = pd.DataFrame(np.random.randn(1000,4), columns=list('ABCD'))
+                ''')
+
+            wait_for_idle(kc)
+            execute(kc=kc, code='%use C++14')
+            wait_for_idle(kc)
+            execute(kc=kc, code='%get dataframe')
+            wait_for_idle(kc)
+
+            execute(kc=kc, code='std::cout << dataframe.size();')
+            stdout, _ = assemble_output(iopub)
+            self.assertEqual(stdout.strip(),'4000')
+
+            execute(kc=kc, code="%use sos")
+            wait_for_idle(kc)
+
 
 if __name__ == '__main__':
     unittest.main()
